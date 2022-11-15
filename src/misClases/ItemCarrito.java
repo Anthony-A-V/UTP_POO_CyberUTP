392d488781c865915e348e9d79ef2487320ccf1f
AnthonyANVA <anthony.anguizvargas@gmail.com>
2022-11-15 01:13:53 -0500

    cambios mates

--- src/misClases/ItemCarrito.java
+++ src/misClases/ItemCarrito.java
@@ -20,25 +20,29 @@ public class ItemCarrito extends javax.swing.JPanel {
      * Creates new form ItemProducto
      */
 
-    public Producto getProducto() {
+    public Producto getProductoCarrito() {
         return producto;
     }
 
-    public void setProducto(Producto producto) {
+    public void setProductoCarrito(Producto producto) {
         this.producto = producto;
-        Producto.setText(producto.getNombreProducto());
+        lblProducto.setText(producto.getNombreProducto());
         lblPrecio.setText("S/. " + precioProductoCarrito());
         lblCantidad.setText(String.valueOf(producto.getCantidad()));
     }
 
-    public void deleteProducto() {
+    public void deleteProductoCarrito() {
         JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
-        ((frmInicioCliente) topFrame).eliminarProductoCarrito(this, precioProductoCarrito());
+        ((frmInicioCliente) topFrame).eliminarProductoCarrito(this);
     }
 
     public double precioProductoCarrito() {
-        double precio = 0;
-        precio = (double) producto.getPrecio() * producto.getCantidad();
+        double precio = producto.getPrecio() * (double) producto.getCantidad();
+        return precio;
+    }
+
+    public double precioProductoCarritoExistente() {
+        double precio = producto.getPrecio() * Double.parseDouble(lblCantidad.getText());
         return precio;
     }
 
@@ -55,10 +59,12 @@ public class ItemCarrito extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        Producto = new javax.swing.JLabel();
+        lblProducto = new javax.swing.JLabel();
         lblPrecio = new javax.swing.JLabel();
         btnEliminar = new javax.swing.JButton();
         lblCantidad = new javax.swing.JLabel();
@@ -66,8 +72,8 @@ public class ItemCarrito extends javax.swing.JPanel {
         setMaximumSize(new java.awt.Dimension(475, 60));
         setPreferredSize(new java.awt.Dimension(453, 40));
 
-        Producto.setFont(new java.awt.Font("Consolas", 0, 12)); // NOI18N
-        Producto.setText("Producto");
+        lblProducto.setFont(new java.awt.Font("Consolas", 0, 12)); // NOI18N
+        lblProducto.setText("Producto");
 
         lblPrecio.setFont(new java.awt.Font("Consolas", 0, 12)); // NOI18N
         lblPrecio.setText("Precio");
@@ -86,39 +92,40 @@ public class ItemCarrito extends javax.swing.JPanel {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addComponent(Producto, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(46, 46, 46)
-                .addComponent(lblPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(lblCantidad)
-                .addGap(59, 59, 59)
-                .addComponent(btnEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(33, 33, 33))
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(24, 24, 24)
+                                .addComponent(lblProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 86,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(46, 46, 46)
+                                .addComponent(lblPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, 89,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(lblCantidad)
+                                .addGap(59, 59, 59)
+                                .addComponent(btnEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 94,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(33, 33, 33)));
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(8, 8, 8)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Producto)
-                    .addComponent(lblPrecio)
-                    .addComponent(btnEliminar)
-                    .addComponent(lblCantidad))
-                .addGap(11, 11, 11))
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(8, 8, 8)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(lblProducto)
+                                        .addComponent(lblPrecio)
+                                        .addComponent(btnEliminar)
+                                        .addComponent(lblCantidad))
+                                .addGap(11, 11, 11)));
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_btnEliminarActionPerformed
-        deleteProducto();
+        deleteProductoCarrito();
     }// GEN-LAST:event_btnEliminarActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Producto;
     private javax.swing.JButton btnEliminar;
     private javax.swing.JLabel lblCantidad;
     private javax.swing.JLabel lblPrecio;
+    private javax.swing.JLabel lblProducto;
     // End of variables declaration//GEN-END:variables
 }
