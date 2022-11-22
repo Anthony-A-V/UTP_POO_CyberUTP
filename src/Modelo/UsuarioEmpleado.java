90519cf4f0e9535ffa6fd18d31dbd64beea67aef
Slimaure <55924768+Slimaure@users.noreply.github.com>
2022-11-22 03:25:09 -0500

    cambios lombok2

--- src/Modelo/UsuarioEmpleado.java
+++ src/Modelo/UsuarioEmpleado.java
@@ -1,36 +1,17 @@
 package Modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class UsuarioEmpleado extends Usuario {
 
     private int idEmpleado;
     private String nombres;
     private String apellidos;
 
-    public UsuarioEmpleado() {
-        super();
-    }
-
-    public int getIdEmpleado() {
-        return idEmpleado;
-    }
-
-    public void setIdEmpleado(int idEmpleado) {
-        this.idEmpleado = idEmpleado;
-    }
-
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
+    //qwe
 }
